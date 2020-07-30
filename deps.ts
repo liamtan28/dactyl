7b741f5b8b8f993f9b511d8f76bda93949d2cba4
Kuncoro Wicaksono <coroo.wicaksono@gmail.com>
2020-07-30 10:51:03 +0700

    :arrow_up: Upgrade std module
    Upgrade std module to latest version:
    :arrow_up: https://deno.land/std@0.62.0

--- deps.ts
+++ deps.ts
@@ -8,6 +8,6 @@ export {
   Response,
   Middleware,
 } from "https://deno.land/x/oak@v4.0.0/mod.ts";
-export { Status, STATUS_TEXT } from "https://deno.land/std@0.51.0/http/http_status.ts";
-export { assertEquals } from "https://deno.land/std@0.51.0/testing/asserts.ts";
-export { green, red, yellow, blue, bgBlue } from "https://deno.land/std@0.51.0/fmt/colors.ts";
+export { Status, STATUS_TEXT } from "https://deno.land/std@0.62.0/http/http_status.ts";
+export { assertEquals } from "https://deno.land/std@0.62.0/testing/asserts.ts";
+export { green, red, yellow, blue, bgBlue } from "https://deno.land/std@0.62.0/fmt/colors.ts";
