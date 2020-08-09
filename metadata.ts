8a0b3e1106a47593c3c5e15d99950cb6ca533ea5
= <=>
2020-08-10 00:31:32 +1000

    :art: ported metadata to use polyfill for DI support

--- metadata.ts
+++ metadata.ts
@@ -4,7 +4,7 @@
  * Metadata utility file - helper methods for assigning and retrieving
  * values from the Reflect API, for use across lib
  */
-
+import { Reflect } from "./injection/reflect-poly.ts";
 import { ControllerMetadata, RouteDefinition } from "./types.ts";
 
 /**
@@ -18,7 +18,7 @@ export const CONTROLLER_META_PROPKEY = Symbol("dactyl_controller_metadata");
  * Helper method for retrieving metadata of controller definition
  */
 export function getControllerOwnMeta(target: Function): ControllerMetadata | undefined {
-  return Reflect.get(target, CONTROLLER_META_PROPKEY);
+  return Reflect.getMetadata(CONTROLLER_META_PROPKEY, target);
 }
 
 /**
@@ -31,7 +31,7 @@ export function getControllerMeta(target: Object): ControllerMetadata | undefine
  * Helper method for setting controller own metadata with a given value.
  */
 export function setControllerOwnMeta(target: Function, value: ControllerMetadata): void {
-  Reflect.defineProperty(target, CONTROLLER_META_PROPKEY, { value });
+  Reflect.defineMetadata(CONTROLLER_META_PROPKEY, value, target);
 }
 
 /**
