5128a47e3d51e4a4c50cdd3d57f1aaa58e08b273
= <=>
2020-09-01 17:40:44 +1000

    :white_check_mark: fixed broken test due to change of Reflect API

--- metadata.test.ts
+++ metadata.test.ts
@@ -1,6 +1,7 @@
 // Copyright 2020 Liam Tan. All rights reserved. MIT license.
 
 import { assertEquals } from "./deps.ts";
+import { Reflect } from "./lib/reflect.ts";
 
 import {
   CONTROLLER_META_PROPKEY,
@@ -10,67 +11,59 @@ import {
   setControllerMeta,
 } from "./metadata.ts";
 import { ControllerMetadata } from "./types.ts";
+import { from } from "https://deno.land/x/evt@1.8.0/lib/Evt.from.ts";
 
 Deno.test({
-  name:
-    "setControllerOwnMeta should assign metadata to the controller meta key",
+  name: "setControllerOwnMeta should assign metadata to the controller meta key",
   fn(): void {
-    class TestClass {
-    }
-    const testMeta: ControllerMetadata = "TEST" as unknown as ControllerMetadata;
+    class TestClass {}
+    const testMeta: ControllerMetadata = ("TEST" as unknown) as ControllerMetadata;
 
     setControllerOwnMeta(TestClass, testMeta);
-    assertEquals(Reflect.get(TestClass, CONTROLLER_META_PROPKEY), testMeta);
+    assertEquals(Reflect.getMetadata(CONTROLLER_META_PROPKEY, TestClass), testMeta);
   },
 });
 
 Deno.test({
-  name:
-    "getControllerOwnMeta should retreive metadata from controller meta key",
+  name: "getControllerOwnMeta should retreive metadata from controller meta key",
   fn(): void {
-    class TestClass {
-    }
-    const testMeta: ControllerMetadata = "TEST" as unknown as ControllerMetadata;
+    class TestClass {}
+    const testMeta: ControllerMetadata = ("TEST" as unknown) as ControllerMetadata;
 
-    Reflect.defineProperty(TestClass, CONTROLLER_META_PROPKEY, {
-      value: testMeta,
-    });
+    Reflect.defineMetadata(CONTROLLER_META_PROPKEY, testMeta, TestClass);
 
     assertEquals(getControllerOwnMeta(TestClass), testMeta);
   },
 });
 
 Deno.test({
-    name: "setControllerMeta should assign metadata to target constructor",
-    fn() {
-        const testMeta: ControllerMetadata = "TEST" as unknown as ControllerMetadata;
-  
-        class TestClass {
-            public testMethod() {
-                setControllerMeta(this, testMeta);
-            }
-        }
-        new TestClass().testMethod();
-        assertEquals(Reflect.get(TestClass, CONTROLLER_META_PROPKEY), testMeta);
+  name: "setControllerMeta should assign metadata to target constructor",
+  fn() {
+    const testMeta: ControllerMetadata = ("TEST" as unknown) as ControllerMetadata;
+
+    class TestClass {
+      public testMethod() {
+        setControllerMeta(this, testMeta);
+      }
     }
+    new TestClass().testMethod();
+    assertEquals(Reflect.getMetadata(CONTROLLER_META_PROPKEY, TestClass), testMeta);
+  },
 });
 
 Deno.test({
-    name: "getControllerMeta should retreive metadata from target constructor",
-    fn() {
-        const testMeta: ControllerMetadata = "TEST" as unknown as ControllerMetadata;
+  name: "getControllerMeta should retreive metadata from target constructor",
+  fn() {
+    const testMeta: ControllerMetadata = ("TEST" as unknown) as ControllerMetadata;
 
-        class TestClass {
-            public testMethod() {
-                assertEquals(getControllerMeta(this), testMeta);
-            }
-        }
-   
-        Reflect.defineProperty(TestClass, CONTROLLER_META_PROPKEY, {
-            value: testMeta,
-        });
-
-        new TestClass().testMethod();
-       
+    class TestClass {
+      public testMethod() {
+        assertEquals(getControllerMeta(this), testMeta);
+      }
     }
+
+    Reflect.defineMetadata(CONTROLLER_META_PROPKEY, testMeta, TestClass);
+
+    new TestClass().testMethod();
+  },
 });
