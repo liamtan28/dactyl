ea1e8b685e6839a90a2dcd739592aaa034963662
= <=>
2020-09-05 19:05:48 +1000

    :white_check_mark: Added units for transform functions

--- transform.test.ts
+++ transform.test.ts
@@ -1,9 +1,47 @@
-import { assertEquals } from "./deps.ts";
+// Copyright 2020 Liam Tan. All rights reserved. MIT license.
+
+import { assertEquals, Context, RouterContext } from "./deps.ts";
 import { retrieveFromContext, buildRouteArgumentsFromMeta, getStatus } from "./transform.ts";
+import { createMockContext, MockContextOptions } from "./utils.ts";
+
+async function retreiveFromMockContext(
+  options: MockContextOptions = {}
+): Promise<{
+  params: any;
+  headers: any;
+  query: any;
+  body: any;
+}> {
+  const context: Context = createMockContext(options);
+  return await retrieveFromContext(<RouterContext>context);
+}
 
 Deno.test({
-  name: "Test true to be true",
-  fn() {
-    assertEquals(true, true);
+  name: "retreiveFromMockContext returns query parameters from context",
+  async fn() {
+    const key = "foo";
+    const value = "bar";
+    const { query } = await retreiveFromMockContext({ path: "/?foo=bar" });
+    assertEquals(query[key], value);
+  },
+});
+Deno.test({
+  name: "retreiveFromMockContext returns params from context",
+  async fn() {
+    const key = "foo";
+    const value = "bar";
+    const { params } = await retreiveFromMockContext({ path: "/", params: { [key]: value } });
+    assertEquals(params[key], value);
+  },
+});
+// Would usually test for `Body` retrieval here but no way to set it in mock,
+// without importing a fair amount of code from Oak. Unfortunate.
+
+Deno.test({
+  name: "retreiveFromMockContext returns headers from context",
+  async fn() {
+    const { headers } = await retreiveFromMockContext({ path: "/" });
+    console.log(headers);
+    assertEquals(headers, {});
   },
 });
--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 // Copyright 2020 Liam Tan. All rights reserved. MIT license.
 
 import { log } from "./deps.ts";
-
+import { Context, Application } from "./deps.ts";
 /**
  * Utility function for throwing an error gracefully without using
  * global top level error exit.
@@ -9,4 +9,50 @@ import { log } from "./deps.ts";
 export function throwCompileTimeError(msg: string, code?: number) {
   log.error(`Compile-time error: ${msg}`);
   Deno.exit(code ?? 1);
+}
+
+/**
+ * TestUtils
+ */
+// Copyright 2018-2020 the oak authors. All rights reserved. MIT license.
+// From Kitson Kelly's Oak Project https://github.com/oakserver/oak
+export interface MockContextOptions<
+  S extends Record<string | number | symbol, any> = Record<string, any>
+> {
+  app?: Application<S>;
+  method?: string;
+  params?: Record<string, string>;
+  path?: string;
+}
+
+export function createMockApp<
+  S extends Record<string | number | symbol, any> = Record<string, any>
+>(state = {} as S): Application<S> {
+  return {
+    state,
+  } as any;
+}
+
+export function createMockContext<
+  S extends Record<string | number | symbol, any> = Record<string, any>
+>({ app = createMockApp(), method = "GET", params, path = "/" }: MockContextOptions = {}) {
+  const headers = new Headers();
+  return ({
+    app,
+    params,
+    request: {
+      headers: new Headers(),
+      method,
+      url: new URL(path, "https://localhost/"),
+    },
+    response: {
+      status: undefined,
+      body: undefined,
+      redirect(url: string | URL) {
+        headers.set("Location", encodeURI(String(url)));
+      },
+      headers,
+    },
+    state: app.state,
+  } as unknown) as Context<S>;
 }
